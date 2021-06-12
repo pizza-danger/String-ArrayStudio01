cd8bb3b9084fa5792d56de765cb8d1195062eeb5
thereyner <33945338+pizza-danger@users.noreply.github.com>
2021-06-12 12:42:52 +0000

    ask the question

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ let strPigBit = str.slice(0,3);
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
 
-console.log(`${str.slice(3,9).concat(strPigBit)}`);
+console.log(`${str.slice(3,10).concat(strPigBit)}`);
 
 //b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-
+strPigBit = input.question("How many letters will be moved? ");
 
 //c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
